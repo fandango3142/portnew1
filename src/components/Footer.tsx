145f8c3305479912311297169242a1898dd4db42
fandango3142 <dukedsa8@gmail.com>
2024-11-06 12:23:19 +0530

    small change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -48,7 +48,7 @@ export default function Footer() {
         </div>
         
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; {new Date().getFullYear()} Portfolio. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} Ryan's Portfolio 2024. All rights reserved.</p>
         </div>
       </div>
     </footer>
